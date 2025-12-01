f0d85747de64848f5f9b01ba0b5ea442ad479f88
LolloLauz <lorenzo.luzi@cooder.it>
2025-12-01 09:38:29 +0100

    fix new extraction

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -76,7 +76,10 @@ function HomePage() {
             senders: senders,
             functions: functions
         }
-        const response = await _sendData(contractName, contractAddress, impl_contract, fromBlock, toBlock, network, smartContract, filters, extractionTypes.indexOf(extractionType))
+        const oldParams = {
+            contractName, contractAddress, impl_contract, fromBlock, toBlock, network, smartContract, filters
+        }
+        const response = await _sendData({oldParams})
         if (response.status === 200) {
             setResults(response.data)
             setLoading(false)
